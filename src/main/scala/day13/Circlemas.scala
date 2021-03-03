f91a7f6eed508df435ad172b607c906372580073
michal fujarewicz <michal.fujarewicz@gmail.com>
2021-03-03 18:49:52 +0100

    formatting

--- src/main/scala/day13/Circlemas.scala
+++ src/main/scala/day13/Circlemas.scala
@@ -5,12 +5,9 @@ import scala.io.Source
 object Circlemas {
   def main(args: Array[String]): Unit = {
 
-
     val source = Source.fromFile("src/main/scala/day13/input.txt")
     val input = source.getLines().toList
     source.close
-
-
 
     val data = input.map(s => s.substring(0, s.length - 1))
       .map(_.split(" "))
@@ -27,25 +24,25 @@ object Circlemas {
     val best1 = findBestScore(stats)
     println(best1)
 
-    for(name <- stats.keys.map(_._1).toList.distinct){
+    for (name <- stats.keys.map(_._1).toList.distinct) {
       stats += (("Me", name) -> 0)
       stats += ((name, "Me") -> 0)
     }
     val best2 = findBestScore(stats)
     println(best2)
   }
-  def findBestScore(stats:Map[(String, String), Int]): Int = {
+
+  def findBestScore(stats: Map[(String, String), Int]): Int = {
     val names = stats.keys.map(_._1).toSet.toList
     val tables = names.permutations.filter(_.head == names.head)
 
     tables.map(t => {
       var score = 0
-      for (i <- t.indices){
-        score = score + stats(t(i), t((i+1)%names.size))
-        score = score + stats(t(i), t((i+names.size-1)%names.size))
+      for (i <- t.indices) {
+        score = score + stats(t(i), t((i + 1) % names.size))
+        score = score + stats(t(i), t((i + names.size - 1) % names.size))
       }
       score
     }).max
-
   }
 }
