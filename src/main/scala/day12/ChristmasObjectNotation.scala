5f0e515ef9b5f5e60d909b583ee7bb590dd3ec46
michal fujarewicz <michal.fujarewicz@gmail.com>
2021-03-05 21:30:03 +0100

    day 12 part 2 solution

--- src/main/scala/day12/ChristmasObjectNotation.scala
+++ src/main/scala/day12/ChristmasObjectNotation.scala
@@ -1,43 +1,69 @@
 package day12
 
 import scala.io.Source
+import scala.util.control.Breaks.{break, breakable}
 
 object ChristmasObjectNotation {
 
-  def findRedObjects(str: String) = {
+  def findRedObjects(str: String): Int = {
 
     val redVal = """:"red""""
+    val strBuilder = new StringBuilder(str)
 
-    var lastObjEnd = -1
+    while (strBuilder.mkString.contains(redVal)) {
+      val redIndex = strBuilder.indexOf(redVal)
 
-    while(str.substring(lastObjEnd+1).contains(redVal)){
-      val currStr = str.substring(lastObjEnd+1)
-      val redIndex = currStr.indexOf(redVal)
-      val objStart = {
-        var i = redIndex
-        while (currStr(i) != '{') i += 1
-        i
-      }
-      val objEnd = {
-        var i = redIndex
-        var bracketCount = 1
-        while (bracketCount > 0) {
-          i += 1
+      var bracketCount = 0
+      var leftBracket = 0
+      breakable(for (i <- redIndex to 0 by -1) {
+        if (strBuilder(i) == '}') {
+          bracketCount += 1
         }
-        i
-      }
+        if (strBuilder(i) == '{') {
 
+          if (bracketCount == 0) {
+            leftBracket = i
+
+            break()
+          }
+          bracketCount -= 1
+        }
+      })
+
+      var rightBracket = 0
+      breakable(for (i <- redIndex until strBuilder.length) {
+        if (strBuilder(i) == '{') {
+
+          bracketCount += 1
+        }
+        if (strBuilder(i) == '}') {
+
+          if (bracketCount == 0) {
+            rightBracket = i
+            break()
+          }
+          bracketCount -= 1
+        }
+      })
+      //      println(strBuilder.subSequence(leftBracket, rightBracket+1))
+      strBuilder.delete(leftBracket, rightBracket + 1)
     }
+
+
+    strBuilder.toString.split(",").map(s => s.filter(c => c.isDigit || c == '-')).filter(_.nonEmpty).map(_.toInt).sum
   }
 
   def main(args: Array[String]): Unit = {
-    val list = Source.fromFile("src/main/scala/day12/input.txt").getLines.mkString
 
-    val  sum = list.split(",").map(s => s.filter(c => c.isDigit || c == '-')).filter(_.nonEmpty).map(_.toInt).sum
+    val source = Source.fromFile("src/main/scala/day12/input.txt")
+    val list = source.getLines.mkString
+    source.close()
+
+    val sum = list.split(",").map(s => s.filter(c => c.isDigit || c == '-')).filter(_.nonEmpty).map(_.toInt).sum
 
     val redSum = findRedObjects(list)
 
-
     println(sum)
+    println(redSum)
   }
 }
